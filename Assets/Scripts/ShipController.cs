03e2b4456e9a3939b86d46e74179fae6bcf9be8e
TomasDobrovolny <b8t0d3r@seznam.cz>
2019-06-05 15:30:38 +0200

    rotation, sprite

--- Assets/Scripts/ShipController.cs
+++ Assets/Scripts/ShipController.cs
@@ -6,19 +6,49 @@ public class ShipController : MonoBehaviourPun
     [SerializeField] private float speed = 5;
 
     private Rigidbody2D rb2d;
-
+    private Camera myCamera;
     public void Start()
     {
+        myCamera = GetComponentInChildren<Camera>();
         rb2d = GetComponent<Rigidbody2D>();
+        if (photonView.IsMine)
+        {
+            myCamera.enabled = true;
+        }
     }
+
+    Vector2 myPosition;
+    Vector2 mousePosition;
 
     public void FixedUpdate()
     {
         if (photonView.IsMine)
         {
+            myPosition = (Vector2)transform.position;
+            mousePosition = (Vector2)(myCamera.ScreenToWorldPoint(Input.mousePosition));
+            print("mp1" + mousePosition);
             var direction = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-
             rb2d.AddForce(direction * speed);
+            transform.eulerAngles = new Vector3(0, 0, GetAngleV2(myPosition, mousePosition));
+            myCamera.transform.eulerAngles = new Vector3(0, 0, 0);
         }
+    }
+
+    float GetAngleV2(Vector2 vec1, Vector2 vec2) {
+        float resu = Mathf.Atan2(vec2.y - vec1.y, vec2.x - vec1.x) * (180f / Mathf.PI);
+        if (vec2.y == 1f)
+        {
+            resu = 90.0f;
+        }
+        if (vec2.y == -1f)
+        {
+            resu = -90.0f;
+        }
+        resu = resu - 90f;
+        if (resu < 0)
+        {
+            resu += 360f;
+        }
+        return resu;
     }
 }
---
+++ Assets/Scripts/SpriteController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class SpriteController : MonoBehaviour
+{
+
+    public bool enableIfMy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (GetComponentInParent<ShipController>().photonView.IsMine)
+        {
+            gameObject.SetActive(enableIfMy);
+        }
+        else
+        {
+            gameObject.SetActive(!enableIfMy);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
