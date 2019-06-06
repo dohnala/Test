ac1b2c8533661d746194372f3a8cbbad7d4ba506
kolisek <richard.kolisek@gmail.com>
2019-06-07 00:10:38 +0200

    * improved movement of ship

--- Assets/Scripts/ShipController.cs
+++ Assets/Scripts/ShipController.cs
@@ -6,7 +6,13 @@ public class ShipController : MonoBehaviourPun
 {
     public float speed = 1;
     public float drag = 0.95f;
-    
+
+    public Transform[] leftThrusters;
+    public Transform[] rightThrusters;
+    public Transform[] frontThrusters;
+
+    public Transform[] engines;
+
     private Rigidbody2D _rb2d;
     
     private float _angle;
@@ -20,31 +26,48 @@ public class ShipController : MonoBehaviourPun
     public void FixedUpdate()
     {
         if (!photonView.IsMine) return;
+
+        float forwardBackwardThrust = Input.GetAxis("Vertical");
+        float leftRightThrust = Input.GetAxis("Horizontal");
+
+        if (leftRightThrust < 0)
+        {
+            ApplyThrusters(leftThrusters, leftRightThrust, transform.right);
+        }
+
+        if (leftRightThrust > 0)
+        {
+            ApplyThrusters(rightThrusters, leftRightThrust, transform.right);
+        }
+
+        if (forwardBackwardThrust < 0)
+        {
+            ApplyThrusters(frontThrusters, forwardBackwardThrust, transform.up);
+        }
+
+        ApplyEngines();
+    }
+
+    private void ApplyEngines()
+    {
+        float forwardThrust = Input.GetAxis("Vertical");
         
-        var cachedTransform = transform;
-            
-        //turn left or right
-        _angle -= Input.GetAxis("Horizontal");
+        foreach (var engine in engines)
+        {
+            if (forwardThrust > 0)
+            {
+                Vector2 enginePos = new Vector2(engine.position.x, engine.transform.position.y);
+                _rb2d.AddForceAtPosition(transform.up * Mathf.Abs(forwardThrust) * 2, enginePos);
+            }
+        }
+    }
 
-        // change object rotation
-        cachedTransform.eulerAngles = new Vector3(0, 0, _angle);
-
-        var cachedEulerAngles = cachedTransform.eulerAngles;
-            
-        // calculate direction from actual ship rotation
-        var direction = new Vector2(
-            -Mathf.Sin(cachedEulerAngles.z * Mathf.Deg2Rad),
-            Mathf.Cos(cachedEulerAngles.z * Mathf.Deg2Rad));
-
-        // accelerate or decelerate
-        _acceleration -= -Input.GetAxis("Vertical") * speed;
-        _acceleration = Math.Max(0, _acceleration);
-
-        // move ship
-        _rb2d.AddForce(_acceleration * direction);
-
-        //slowing down when no force to rb2d
-        _rb2d.velocity *= drag;
-        _acceleration *= drag;
+    private void ApplyThrusters(Transform[] thrusters, float thrust, Vector3 direction)
+    {
+        foreach (var thruster in thrusters)
+        {
+            Vector2 thrusterPos = new Vector2(thruster.position.x, thruster.transform.position.y);
+            _rb2d.AddForceAtPosition(direction * thrust, thrusterPos);
+        }
     }
 }
