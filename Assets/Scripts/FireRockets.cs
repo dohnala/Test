49bfc3400a2ef4ba80ab7c7ea4adec65ce1a6b7a
dohnal <dohnal.adam@gmail.com>
2019-06-06 18:52:42 +0200

    Add support for firing rockets

---
+++ Assets/Scripts/FireRockets.cs
@@ -0,0 +1,28 @@
+﻿using Photon.Pun;
+using UnityEngine;
+
+public class FireRockets : MonoBehaviourPun
+{
+    public GameObject rocketPrefab;
+
+    public Transform[] spawnPoints;
+    
+    public void Update()
+    {
+        if (!photonView.IsMine) return;
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            photonView.RPC("FireBullets", RpcTarget.All);
+        }
+    }
+    
+    [PunRPC]
+    public void FireBullets()
+    {
+        foreach (var spawnPoint in spawnPoints)
+        {
+            Instantiate(rocketPrefab, spawnPoint.position, spawnPoint.rotation);
+        }    
+    }
+}
---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,30 @@
+﻿using Photon.Pun;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    public float speed = 0.1f;
+    public float duration = 1;
+
+    public void Start()
+    {
+        Destroy(gameObject, duration);
+    }
+
+    public void FixedUpdate()
+    {
+        transform.Translate(0, speed, 0, Space.Self);
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        var otherPhotonView = other.gameObject.GetComponent<PhotonView>();
+
+        if (otherPhotonView != null && otherPhotonView.IsMine)
+        {
+            // Take damage via RPC
+        }
+
+        Destroy(gameObject);
+    }
+}
