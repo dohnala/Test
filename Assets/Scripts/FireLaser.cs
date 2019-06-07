f7a68058d877b8a3708b8efbdf6c6de7682583a5
kolisek <richard.kolisek@gmail.com>
2019-06-07 15:05:23 +0200

    * add laser weapon

---
+++ Assets/Scripts/FireLaser.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+
+public class FireLaser : MonoBehaviourPun
+{
+    public GameObject laserPrefab;
+    public GameObject collisionEffect;
+
+    public Transform[] spawnPoints;
+
+    public void Update()
+    {
+        if (!photonView.IsMine) return;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            photonView.RPC("FireLasers", RpcTarget.All);
+        }
+    }
+
+    [PunRPC]
+    public void FireLasers()
+    {
+        foreach (var spawnPoint in spawnPoints)
+        {
+            var laser = Instantiate(laserPrefab, spawnPoint.position, spawnPoint.rotation);
+
+            laser.GetComponent<Laser>().SpawnPoint = spawnPoint;
+            laser.GetComponent<Laser>().CollisionEffect = collisionEffect;
+            
+            Destroy(laser, 2.0f);
+        }
+    }
+}
---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    private static float effectTime = 0.1f;
+    private LineRenderer lr;
+    public Transform SpawnPoint{ private get; set; }
+    
+    public GameObject CollisionEffect { private get; set; }
+
+    public float maxDistance;
+    
+    void Start()
+    {
+        lr = GetComponent<LineRenderer>();
+    }
+
+    void Update()
+    {
+        var position = SpawnPoint.position;
+        var position2d = new Vector2(position.x, position.y);
+        var up = SpawnPoint.up;
+
+        lr.SetPosition(0, position);
+
+        RaycastHit2D hit = Physics2D.Raycast(position2d, up, maxDistance);
+        
+        if (hit.collider != null)
+        {
+            lr.SetPosition(1, hit.point);
+            
+            GameObject effect = Instantiate(CollisionEffect, hit.point, hit.transform.rotation);
+            Destroy(effect, effectTime);
+        }
+        else
+        {
+            lr.SetPosition(1, position + (up * maxDistance));
+        }
+        
+    }
+}
