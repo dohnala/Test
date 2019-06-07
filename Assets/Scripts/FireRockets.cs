f62af53d8b2c5732fc41c602d8071f366df00e2f
dohnal <dohnal.adam@gmail.com>
2019-06-07 11:07:34 +0200

    Rocket should ignore collision with its owner

--- Assets/Scripts/FireRockets.cs
+++ Assets/Scripts/FireRockets.cs
@@ -6,32 +6,31 @@ public class FireRockets : MonoBehaviourPun
     public GameObject rocketPrefab;
 
     public Transform[] spawnPoints;
-    
+
     public void Update()
     {
         if (!photonView.IsMine) return;
-        
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
             photonView.RPC("FireBullets", RpcTarget.All);
         }
     }
-    
+
     [PunRPC]
     public void FireBullets()
     {
         var ownerVelocity = gameObject.GetComponent<Rigidbody2D>().velocity;
-        
+
         foreach (var spawnPoint in spawnPoints)
         {
             var rocket = Instantiate(rocketPrefab, spawnPoint.position, spawnPoint.rotation);
 
-            Debug.Log("Owner velocity " + ownerVelocity);
-            
+            // pass this game object as an owner of the rocket
+            rocket.GetComponent<Rocket>().Owner = gameObject;
+
             // rocket should start with the same velocity as owner
             rocket.GetComponent<Rigidbody2D>().velocity += ownerVelocity;
-            
-            Debug.Log("Rocket velocity " + rocket.GetComponent<Rigidbody2D>().velocity);
-        }    
+        }
     }
 }
--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -6,24 +6,27 @@ public class Rocket : MonoBehaviour
     public float speed = 5;
     public float duration = 2;
 
+    public GameObject Owner { private get; set; }
+
     public void Start()
     {
         var direction = transform.up;
-        
+
         GetComponent<Rigidbody2D>().velocity += speed * new Vector2(direction.x, direction.y);
 
-        Debug.Log("Rocket velocity " + GetComponent<Rigidbody2D>().velocity);
-        
         Destroy(gameObject, duration);
     }
 
     public void OnTriggerEnter2D(Collider2D other)
     {
+        // ignore collision with owner
+        if (other.gameObject == Owner) return;
+
         var otherPhotonView = other.gameObject.GetComponent<PhotonView>();
 
         if (otherPhotonView != null && otherPhotonView.IsMine)
         {
-            // Take damage via RPC
+            // take damage via RPC
         }
 
         Destroy(gameObject);
