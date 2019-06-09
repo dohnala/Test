c61eb28346030dce002575c6285b82327a0514a6
dohnal <dohnal.adam@gmail.com>
2019-06-09 10:57:21 +0200

    Add player HUD

---
+++ Assets/Scripts/UI/PlayerHUD.cs
@@ -0,0 +1,14 @@
+﻿using UI;
+using UnityEngine;
+
+public class PlayerHUD : MonoBehaviour
+{
+    public HealthBar healthBar;
+
+    public HealthBar HealthBar => healthBar;
+
+    public void SetActive(bool value)
+    {
+        gameObject.SetActive(value);
+    }
+}
