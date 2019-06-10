e605c000f8a8fbadae456ef9f4a833d1118cb7d2
kolisek <richard.kolisek@gmail.com>
2019-06-10 20:42:32 +0200

    * new parallax script

---
+++ Assets/Scripts/Parralax.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using Vector2 = UnityEngine.Vector2;
+using Vector3 = UnityEngine.Vector3;
+
+public class Parralax : MonoBehaviour
+{
+    public float speedCoefficient = 0.5f;
+
+    void FixedUpdate()
+    {
+        Rigidbody2D shipBody = Ship.Player.GetComponent<Rigidbody2D>();
+        Vector2 speed = (speedCoefficient * Time.deltaTime * shipBody.velocity);
+            
+        transform.position = new Vector3(transform.position.x + speed.x, transform.position.y + speed.y);
+    }
+}
