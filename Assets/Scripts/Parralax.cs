614df218a1b22a7e3b112791f99c25d3ae2a2e74
dohnal <dohnal.adam@gmail.com>
2019-06-11 08:30:54 +0200

    Clean code + refactoring

--- Assets/Scripts/Parralax.cs
+++ Assets/Scripts/Parralax.cs
@@ -1,20 +1,16 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Numerics;
-using UnityEngine;
-using Vector2 = UnityEngine.Vector2;
-using Vector3 = UnityEngine.Vector3;
+﻿using UnityEngine;
 
 public class Parralax : MonoBehaviour
 {
     public float speedCoefficient = 0.5f;
 
-    void FixedUpdate()
+    private void FixedUpdate()
     {
-        Rigidbody2D shipBody = Ship.Player.GetComponent<Rigidbody2D>();
-        Vector2 speed = (speedCoefficient * Time.deltaTime * shipBody.velocity);
-            
-        transform.position = new Vector3(transform.position.x + speed.x, transform.position.y + speed.y);
+        var cachedPosition = transform.position;
+
+        var shipBody = Ship.Player.GetComponent<Rigidbody2D>();
+        var speed = speedCoefficient * Time.deltaTime * shipBody.velocity;
+
+        transform.position = new Vector3(cachedPosition.x + speed.x, cachedPosition.y + speed.y);
     }
 }
