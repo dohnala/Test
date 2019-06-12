afd15945f7bb07e2a782749fe20e5c91a445e0e0
b8t0d3r <b8t0d3r@seznam.cz>
2019-06-12 22:34:55 +0200

    https://stackoverflow.com/questions/35785291/getting-giggly-effect-when-slowly-moving-a-sprite/35829604
    https://stackoverflow.com/questions/35785291/getting-giggly-effect-when-slowly-moving-a-sprite/35829604

---
+++ Assets/Resources/UI/GUIplacement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GUIplacement : MonoBehaviour
+{
+    public GameObject source;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.SetParent(FindObjectOfType<Canvas>().transform);
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        print(source.GetComponentInChildren<SpriteRenderer>().bounds.size.z);
+        transform.position = Ship.Player.shipCamera.GetComponent<Camera>().WorldToScreenPoint(source.transform.TransformPoint(Vector3.zero -Vector3.Scale(source.transform.localScale,new Vector3(2,-2,0))));
+    }
+}
